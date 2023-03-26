d456fd4810d22cba24a3d8e56ccc163e0f308e6d
p-nuts5963 <polythene.pam5963@gimail.com>
2023-03-26 11:55:40 +0900

    エラーページの設定

--- app.js
+++ app.js
@@ -20,24 +20,15 @@ const router = express.Router();
 app.use("/survey", require("./routes/survey.js"));
 app.use("/", require("./routes/index.js"));
 
-// viewの接続確認
-app.get("/view", (req, res) => {
-  let message = "hello world"
-  res.render("./template.ejs");
-})
-
-// DB接続テスト
-app.get("/test", async (req, res) => {
-  try {
-    results = await MySQLClient.executeQuery(
-      await sql("SELECT_TOPICS"),
-      []
-    );
-    res.send(results);
-  } catch (err) {
-    res.send(err);
-  }
-})
+// エラーページ
+app.use((req, res, next) => {
+  res.status(404);
+  res.render("./404.ejs");
+});
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.render("./500.ejs");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("サーバーが起動しました");
